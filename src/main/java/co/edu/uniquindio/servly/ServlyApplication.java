4e8d013430d6527658ea9ae54e2e879403d9d313
Juan David Ramírez <141882961+JuamchoOrion@users.noreply.github.com>
2026-02-14 14:48:52 -0500

    First Project Structure, BackEnd

---
+++ src/main/java/co/edu/uniquindio/servly/ServlyApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.servly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServlyApplication.class, args);
+	}
+
+}
